5bd9721ea9d75b1bfdea6f53b28333f76bac7765
NorisukiNZk <born.waffa@gmail.com>
2023-05-03 21:46:59 +0800

    Operator Aritmatika

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,15 @@
+package Operator;
+
+public class OperatorBinary {
+    public static void main(String[] args) {
+        String nama = "Muhammad Hasani Waffa";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.89;
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%2);
+        System.out.println(nama+" Jumlah SKS yang diambil: "+jumlahSks+" dengan nilai= "+indeksPrestasiKumulatif);
+    }
+}
