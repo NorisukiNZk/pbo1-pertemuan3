e28175bf64466c7b12a19ce18e272722b2579565
NorisukiNZk <born.waffa@gmail.com>
2023-05-03 22:00:49 +0800

    If Else

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,13 +3,15 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 53000;
-        double uangDiDompet = 54000;
+        double uangDiDompet = 25000;
+
         if(uangDiDompet<totalBelanja){
-            System.out.println("Uang Kurang, Kurangi belanja anda");
-        } else if (uangDiDompet==totalBelanja) {
-            System.out.println("Uang Pas tidak ada Kembalian");
+            System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : " + angsul);
         }else{
-            System.out.println("Uang lebih ada kembalian sebesar "+(uangDiDompet-totalBelanja));
+            System.out.println("Uang pas... UwU..");
         }
     }
 }
